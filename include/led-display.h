7d4496c5c6ddcdec009e0935b9f1a0ea43964392
Paul Burton <paul.burton@imgtec.com>
2015-09-15 15:55:18 -0700

    LCD display API improvements
    Allow use of multi-line displays, and provide wrappers for code to write
    to the display.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>
    (cherry picked from commit 95bbfad5068ab6c26ebb34e8530b83296ab1ccd0)

--- include/led-display.h
+++ include/led-display.h
@@ -13,7 +13,21 @@
 /* Display Commands */
 #define DISPLAY_CLEAR	0x1 /* Clear the display */
 #define DISPLAY_HOME	0x2 /* Set cursor at home position */
+#define DISPLAY_LINE2	0x4 /* Move to line 2 */
 
 void display_set(int cmd);
 int display_putc(char c);
+
+static inline void display_puts(const char *str)
+{
+	while (str[0])
+		display_putc(*str++);
+}
+
+static inline void display_sets(const char *str)
+{
+	display_set(DISPLAY_CLEAR | DISPLAY_HOME);
+	display_puts(str);
+}
+
 #endif
