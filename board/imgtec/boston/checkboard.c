f1140960761ebd0703b634737f7aac756f7d98f0
Paul Burton <paul.burton@imgtec.com>
2016-11-20 14:07:31 +0000

    boston: Use MIPS CPU driver
    Use the MIPS CPU driver to output the CPU name during boot, and enable
    the cpu command to allow the user to see CPU information from the
    command prompt.
    
    CONFIG_SYS_MALLOC_F_LEN is increased in the boston defconfig files as we
    fail to allocate data for the CPU driver with the default 1KB.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

--- board/imgtec/boston/checkboard.c
+++ board/imgtec/boston/checkboard.c
@@ -12,19 +12,55 @@
 #include "boston-lcd.h"
 #include "boston-regs.h"
 
+ulong cpu_mips_rtl_changelist(struct udevice *dev)
+{
+	ulong cl, cl_hex, digit;
+	int nibble, i;
+
+	cl_hex = __raw_readl((uint32_t *)BOSTON_PLAT_CORE_CL);
+
+	/*
+	 * For some bizarre reason the register contains the changelist when a
+	 * hexadecimal display of its value is interpreted as decimal. Convert
+	 * that to a standard integer.
+	 */
+	cl = 0;
+	for (nibble = 0; nibble < (sizeof(ulong) * 2); nibble++) {
+		digit = (cl_hex >> (nibble * 4)) & 0xf;
+		for (i = 0; i < nibble; i++)
+			digit *= 10;
+		cl += digit;
+	}
+
+	return cl;
+}
+
+void cpu_mips_rtl_misc(struct udevice *dev, char *buf, int size)
+{
+	u32 cfg = readl((u32 *)BOSTON_PLAT_BUILDCFG0);
+	int idx = 0;
+
+	if (cfg & (BOSTON_PLAT_BUILDCFG0_CFG_NUM | BOSTON_PLAT_BUILDCFG0_CFG_LTR))
+		idx += snprintf(&buf[idx], size - idx, " config ");
+
+	if (cfg & BOSTON_PLAT_BUILDCFG0_CFG_NUM)
+		idx += snprintf(&buf[idx], size - idx, "%u",
+				(cfg & BOSTON_PLAT_BUILDCFG0_CFG_NUM) >> 8);
+
+	if (cfg & BOSTON_PLAT_BUILDCFG0_CFG_LTR)
+		idx += snprintf(&buf[idx], size - idx, "%c",
+				'a' + ((cfg & BOSTON_PLAT_BUILDCFG0_CFG_LTR) >> 4) - 1);
+
+	if (cfg & BOSTON_PLAT_BUILDCFG0_DP)
+		idx += snprintf(&buf[idx], size - idx, ", x%u debug port",
+				(cfg & BOSTON_PLAT_BUILDCFG0_DP_MULT) >> 28);
+}
+
 int checkboard(void)
 {
-	u32 changelist;
-
 	lowlevel_display("U-boot  ");
 
 	printf("Board: MIPS Boston\n");
-
-	printf("CPU:   0x%08x", read_c0_prid());
-	changelist = __raw_readl((uint32_t *)BOSTON_PLAT_CORE_CL);
-	if (changelist > 1)
-		printf(" cl%x", changelist);
-	putc('\n');
 
 	return 0;
 }
