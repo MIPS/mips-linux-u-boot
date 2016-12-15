90ba7b4d3885f73852ebd2f9b63338c5b12f9319
Paul Burton <paul.burton@imgtec.com>
2016-12-15 13:27:06 +0000

    boston: Support fat binaries
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

---
+++ board/imgtec/boston/fat.mk
@@ -0,0 +1,23 @@
+#
+# SPDX-License-Identifier:	GPL-2.0+
+#
+
+FAT_BASE := 0x9fc00000
+
+FAT_BUILDS += boston32r2
+FAT_BASE_boston32r2 := 0x9fc01000
+
+FAT_BUILDS += boston32r2el
+FAT_BASE_boston32r2el := 0x9fca0000
+
+FAT_BUILDS += boston32r6
+FAT_BASE_boston32r6 := 0x9fd40000
+
+FAT_BUILDS += boston32r6el
+FAT_BASE_boston32r6el := 0x9fde0000
+
+FAT_BUILDS += boston64r6
+FAT_BASE_boston64r6 := 0xffffffff9fe80000
+
+FAT_BUILDS += boston64r6el
+FAT_BASE_boston64r6el := 0xffffffff9ff20000
