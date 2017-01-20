cac263d00b76ba95e5408652516600a609272a61
Paul Burton <paul.burton@imgtec.com>
2017-01-20 15:28:43 -0800

    malta: Add MIPSr6 defconfigs
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

--- board/imgtec/malta/fat.mk
+++ board/imgtec/malta/fat.mk
@@ -9,3 +9,9 @@ FAT_BASE_malta := 0x9fc01000
 
 FAT_BUILDS += maltael
 FAT_BASE_maltael := 0x9fc80000
+
+FAT_BUILDS += malta32r6el
+FAT_BASE_malta32r6el := 0x9fd00000
+
+FAT_BUILDS += malta64r6el
+FAT_BASE_malta64r6el := 0xffffffff9fd80000
