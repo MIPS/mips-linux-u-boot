f2ed8e825832d085355b35fff73cba2879f8c234
Paul Burton <paul.burton@imgtec.com>
2017-01-20 10:17:25 -0800

    malta: Limit relocation to top of kseg0
    Prevent U-Boot from attempting to relocate beyond the DDR accessible via
    kseg0 on systems which have more than 256MB DDR fitted.
    
    Signed-off-by: Paul Burton <paul.burton@imgtec.com>

--- board/imgtec/malta/malta.c
+++ board/imgtec/malta/malta.c
@@ -7,6 +7,7 @@
 
 #include <common.h>
 #include <ide.h>
+#include <linux/sizes.h>
 #include <netdev.h>
 #include <pci.h>
 #include <pci_gt64120.h>
@@ -97,6 +98,18 @@ phys_size_t initdram(int board_type)
 	sz *= 8;
 
 	return sz;
+}
+
+ulong board_get_usable_ram_top(ulong total_size)
+{
+	DECLARE_GLOBAL_DATA_PTR;
+
+	if (gd->ram_top < (ulong)phys_to_virt(CONFIG_SYS_SDRAM_BASE)) {
+		/* 2GB wrapped around to 0 */
+		return (ulong)phys_to_virt(SZ_256M);
+	}
+
+	return min_t(ulong, gd->ram_top, (ulong)phys_to_virt(SZ_256M));
 }
 
 int checkboard(void)
