5f3f2982a7f13dc7a20bb50e3156e43ef27b59f8
Paul Burton <paul.burton@mips.com>
2017-12-11 13:00:56 -0800

    boston: Adjust .mcs addresses based on text base
    TODO: This is ugly; find a nicer way
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>

--- board/imgtec/boston/config.mk
+++ board/imgtec/boston/config.mk
@@ -5,8 +5,10 @@
 quiet_cmd_srec_cat = SRECCAT $@
       cmd_srec_cat = srec_cat -output $@ -$2 $< -binary -offset $3
 
+fl_base = 0x98000000
+
 u-boot.mcs: u-boot.bin
-	$(call cmd,srec_cat,intel,0x7c00000)
+	$(call cmd,srec_cat,intel,$(shell $(shell grep CONFIG_SYS_TEXT_BASE= $(KCONFIG_CONFIG)); echo $$(($${CONFIG_SYS_TEXT_BASE} - $(fl_base)))))
 
 # if srec_cat is present build u-boot.mcs by default
 ifneq ($(shell which srec_cat 2>/dev/null),)
