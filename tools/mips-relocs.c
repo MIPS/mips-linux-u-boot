7ad7ca8348d2965fe2dbd4c67ab10a8de5c6fa0d
Paul Burton <paul.burton@mips.com>
2017-12-12 17:53:39 -0800

    mips-relocs: Bodge to build with glibc 2.5
    The grid machines use an ancient CentOS 5.7 distribution with glibc 2.5
    which doesn't include the endian-conversion macros the mips-relocs tool
    relies upon. Open-code them to get the build to work...
    
    Signed-off-by: Paul Burton <paul.burton@mips.com>

--- tools/mips-relocs.c
+++ tools/mips-relocs.c
@@ -8,6 +8,7 @@
 
 #include <assert.h>
 #include <elf.h>
+#include <endian.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
@@ -18,6 +19,37 @@
 #include <unistd.h>
 
 #include <asm/relocs.h>
+
+#ifndef be16toh
+# include <bits/byteswap.h>
+# if __BYTE_ORDER == __LITTLE_ENDIAN
+#  define be16toh(x)	__bswap_16(x)
+#  define be32toh(x)	__bswap_32(x)
+#  define be64toh(x)	__bswap_64(x)
+#  define le16toh(x)	(x)
+#  define le32toh(x)	(x)
+#  define le64toh(x)	(x)
+#  define htobe16(x)	__bswap_16(x)
+#  define htobe32(x)	__bswap_32(x)
+#  define htobe64(x)	__bswap_64(x)
+#  define htole16(x)	(x)
+#  define htole32(x)	(x)
+#  define htole64(x)	(x)
+# else
+#  define be16toh(x)	(x)
+#  define be32toh(x)	(x)
+#  define be64toh(x)	(x)
+#  define le16toh(x)	__bswap_16(x)
+#  define le32toh(x)	__bswap_32(x)
+#  define le64toh(x)	__bswap_64(x)
+#  define htobe16(x)	(x)
+#  define htobe32(x)	(x)
+#  define htobe64(x)	(x)
+#  define htole16(x)	__bswap_16(x)
+#  define htole32(x)	__bswap_32(x)
+#  define htole64(x)	__bswap_64(x)
+# endif
+#endif
 
 #define hdr_field(pfx, idx, field) ({				\
 	uint64_t _val;						\
