42ac8f194c798317656ecff6a272f5daa0e720c3
Dmitry Korotin <dkorotin@wavecomp.com>
2019-04-19 18:50:20 -0700

    pathing kernel device tree for the specific board added.

--- board/imgtec/boston/boston-regs.h
+++ board/imgtec/boston/boston-regs.h
@@ -8,8 +8,18 @@
 
 #include <asm/addrspace.h>
 
+#define BOSTON_PCIE0_BASE		CKSEG1ADDR(0x10000000)
 #define BOSTON_PLAT_BASE		CKSEG1ADDR(0x17ffd000)
 #define BOSTON_LCD_BASE			CKSEG1ADDR(0x17fff000)
+
+/*
+ * Xilinx AXI Bridge for PCI Express Registers
+ */
+#define XLNX_PCIE_PSCR			0x144
+#define  XLNX_PCIE_PSCR_LINK_UP		(0x1 << 11)
+#define XLNX_PCIE_RPSC			0x148
+#define  XLNX_PCIE_RPSC_BRIDGE_EN	0x1
+#define XLNX_PCIE_CFG_BUS_SHIFT		20
 
 /*
  * Platform Register Definitions
@@ -19,7 +29,25 @@
 #define BOSTON_PLAT_DDR3STAT		(BOSTON_PLAT_BASE + 0x14)
 # define BOSTON_PLAT_DDR3STAT_CALIB	(1 << 2)
 
+#define BOSTON_PLAT_BUILDCFG0		(BOSTON_PLAT_BASE + 0x34)
+# define BOSTON_PLAT_BUILDCFG0_IOCU	(0x1 << 0)
+# define BOSTON_PLAT_BUILDCFG0_PCIE0	(0x1 << 1)
+# define BOSTON_PLAT_BUILDCFG0_PCIE1	(0x1 << 2)
+# define BOSTON_PLAT_BUILDCFG0_PCIE2	(0x1 << 3)
+# define BOSTON_PLAT_BUILDCFG0_CFG_LTR	(0xf << 4)
+# define BOSTON_PLAT_BUILDCFG0_CFG_NUM	(0xff << 8)
+# define BOSTON_PLAT_BUILDCFG0_DP	(0x1 << 24)
+# define BOSTON_PLAT_BUILDCFG0_DP_MULT	(0xf << 28)
+
 #define BOSTON_PLAT_DDRCONF0		(BOSTON_PLAT_BASE + 0x38)
 # define BOSTON_PLAT_DDRCONF0_SIZE	(0xf << 0)
+
+#define BOSTON_PLAT_NOCPCIE0ADDR	(BOSTON_PLAT_BASE + 0x3c)
+#define BOSTON_PLAT_NOCPCIE1ADDR	(BOSTON_PLAT_BASE + 0x40)
+#define BOSTON_PLAT_NOCPCIE2ADDR	(BOSTON_PLAT_BASE + 0x44)
+
+#ifndef __ASSEMBLY__
+extern bool detect_sleipnir(void);
+#endif
 
 #endif /* __BOARD_BOSTON_REGS_H__ */
--- board/imgtec/boston/dt.c
+++ board/imgtec/boston/dt.c
@@ -1,16 +1,112 @@
-// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2016 Imagination Technologies
+ *
+ * SPDX-License-Identifier:	GPL-2.0
  */
 
 #include <common.h>
 #include <fdt_support.h>
+#include <asm/io.h>
+#include "boston-regs.h"
+
+static const char sleipnir_compat[] = "img,boston-sleipnir\0img,boston";
+
+const void *get_best_fit_compat(const void *fdt, int *lenp)
+{
+	if (detect_sleipnir()) {
+		if (lenp)
+			*lenp = sizeof(sleipnir_compat);
+		return sleipnir_compat;
+	}
+
+	return fdt_getprop(fdt, 0, "compatible", lenp);
+}
 
 int ft_board_setup(void *blob, bd_t *bd)
 {
 	DECLARE_GLOBAL_DATA_PTR;
 	u64 mem_start[2], mem_size[2];
-	int mem_regions;
+	int mem_regions, off;
+	bool coherent, enabled;
+	ulong phys;
+	u32 upper, build_cfg, ddr_cfg;
+	const fdt32_t *reg;
+	fdt32_t prop_val;
+
+	build_cfg = readl((u32 *)BOSTON_PLAT_BUILDCFG0);
+
+	off = fdt_node_offset_by_compatible(blob, -1, "xlnx,axi-pcie-host-1.00.a");
+	while (off != -FDT_ERR_NOTFOUND) {
+		coherent = false;
+		enabled = false;
+
+		reg = fdt_getprop(blob, off, "reg", NULL);
+		if (reg) {
+			phys = fdt_translate_address(blob, off, reg);
+			switch (phys) {
+			case 0x10000000: /* PCIe0 */
+				upper = readl((u32 *)BOSTON_PLAT_NOCPCIE0ADDR);
+
+				if (detect_sleipnir())
+					enabled = false;
+				else
+					enabled = build_cfg & BOSTON_PLAT_BUILDCFG0_PCIE0;
+				break;
+
+			case 0x12000000: /* PCIe1 */
+				upper = readl((u32 *)BOSTON_PLAT_NOCPCIE1ADDR);
+				enabled = build_cfg & BOSTON_PLAT_BUILDCFG0_PCIE1;
+				break;
+
+			case 0x14000000: /* PCIe2 */
+				upper = readl((u32 *)BOSTON_PLAT_NOCPCIE2ADDR);
+				enabled = build_cfg & BOSTON_PLAT_BUILDCFG0_PCIE2;
+				break;
+
+			default:
+				upper = 0;
+			}
+
+			coherent = upper == 0x10;
+		}
+
+		if (coherent) {
+			prop_val = cpu_to_fdt32(1);
+			fdt_setprop(blob, off, "dma-coherent", &prop_val, sizeof(prop_val));
+		} else {
+			fdt_delprop(blob, off, "dma-coherent");
+		}
+
+		fdt_setprop_string(blob, off, "status",
+				   enabled ? "okay" : "disabled");
+
+		off = fdt_node_offset_by_compatible(blob, off, "xlnx,axi-pcie-host-1.00.a");
+	}
+
+	/*
+	 * Musket adds 0x80000000 to addresses from Xilinx PCIe DMA masters on
+	 * some systems (in order to expand the accessible DDR beyond that
+	 * available in the 32 bit CPU physical address map, with only a 32 bit
+	 * DMA address bus). Indicate that adjustment in dma-ranges.
+	 */
+	ddr_cfg = readl((u32 *)BOSTON_PLAT_DDRCONF0);
+	if (ddr_cfg & (1 << 16)) {
+		fdt32_t dma_ranges[6];
+
+		/* DMA address zero */
+		dma_ranges[0] = cpu_to_fdt32(0);
+		dma_ranges[1] = cpu_to_fdt32(0);
+
+		/* Corresponds to CPU physical address... */
+		dma_ranges[2] = cpu_to_fdt32(0);
+		dma_ranges[3] = cpu_to_fdt32(0x80000000);
+
+		/* With a size matching that of DDR */
+		dma_ranges[4] = cpu_to_fdt32(gd->ram_size >> 32);
+		dma_ranges[5] = cpu_to_fdt32(gd->ram_size);
+
+		fdt_setprop(blob, 0, "dma-ranges", &dma_ranges, sizeof(dma_ranges));
+	}
 
 	mem_start[0] = 0;
 	mem_size[0] = min_t(u64, 256llu << 20, gd->ram_size);
